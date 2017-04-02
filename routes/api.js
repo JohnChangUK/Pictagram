fffe8a92f9ae8572b4f00e432c2d73aaba8cf22b
John Chang <johnchang9094@gmail.com>
2017-04-03 00:33:47 +0100

    Implemented Promise functionality, created forms with post routes and actions, can find data with findById method, by inserting the ID number which was generated by mongo

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,97 @@
 var express = require('express');
 var router = express.Router();
-
+var controllers = require('../controllers');
 
 router.get('/:resource', function(req, res, next) {
     var resource = req.params.resource;
-    
-  res.json({
-    confirmation: 'success',
-    resource: resource
-  });
 
+    var controller = controllers[resource];
+    if (controller == null) {
+      res.json({
+        confirmation: 'Failure... Invalid Resource!',
+        message: 'Invalid Resource'
+      });
+
+      return;
+    }
+
+    controller.get(req.query, false)
+    .then(function(results) {
+      res.json({
+        confirmation: 'Success!',
+        results: results
+      });
+    })
+    .catch(function(err) {
+      res.json({
+        confirmation: 'fail',
+        message: err
+      });
+    });
+});
+
+router.get('/:resource/:id', function(req, res, next) {
+    var resource = req.params.resource;
+
+    var controller = controllers[resource];
+    if (controller == null) {
+      res.json({
+        confirmation: 'Failure... Invalid Resource!',
+        message: 'Invalid Resource'
+      });
+
+      return;
+    }
+
+    controller.getById(req.params.id, false)
+    .then(function(result) {
+      res.json({
+        confirmation: 'Success!',
+        result: result
+      });
+    })
+    .catch(function(err) {
+      res.json({
+        confirmation: 'fail',
+        message: err // this is the error given back by the promise 'reject'
+      });
+    });   
+});
+
+router.post('/:resource', function(req, res, next) {
+
+    var resource = req.params.resource;
+
+    var controller = controllers[resource];
+    if (controller == null) {
+      res.json({
+        confirmation: 'Failure... Invalid Resource!',
+        message: 'Invalid Resource'
+      });
+
+      return;
+    }
+// Alll post request deliver data in the Body, 'req.body'
+    controller.post(req.body, false)
+    .then(function(result) {
+      res.json({
+        confirmation: 'Success!',
+        result: result
+      });
+    })
+    .catch(function(err) {
+      res.json({
+        confirmation: 'Failure... Invalid Resource!',
+        message: err
+      });
+    });
 });
 
 module.exports = router;
+
+
+
+
+
+
+
